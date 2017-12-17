954d7821e438fdffe8235f27977644d914cea65f
Rahul Dhar <rahul.dhar@alepo.com>
2017-12-17 20:26:10 +0530

    Modified the survey route function to fetch unique responses from sendGrip api

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } =require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -11,7 +14,29 @@ module.exports = app => {
     res.send('Thanks for voting!');
   });
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    /**
+     * Mapping through the events and removing all those events
+     * that dont have a surveyID or a choice in the pathname
+     */
+    const events = _.map(req.body, ({email, url}) => {
+      const pathname = new URL (url).pathname;
+      const p = new Path('/api/surveys/:surveyID/:choice');
+      const match = p.test(pathname);
+      if(match){
+        return { 
+          email,
+          surveyID: match.surveyID,
+          choice: match.choice
+        };
+      }
+    });
+
+    //takes an array, removes undefined elements
+    const compactEvents = _.compact(events);
+
+    //fetch unique events based on email and surveyIDs
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyID');
+
     res.send({});
   });
   app.post('/api/surveys', requireLogin, requireCredits,  async (req, res) => {
