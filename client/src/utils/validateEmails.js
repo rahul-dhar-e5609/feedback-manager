2827c6c84f92c7d507d1360ef2c26e580bc6f7ef
Rahul Dhar <rahul.dhar@alepo.com>
2017-12-17 19:48:26 +0530

    removed whitespace

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -7,7 +7,6 @@ export default (emails) => {
         .map(email => email.trim())
         .filter(email => re.test(email) === false);//keeping invalid emails here
         //filter function, if you return true, value will be kept inside the array
-
     console.log("Invalid Emails", invalidEmails);
     if(invalidEmails.length){
         return `These emails are invalid: ${invalidEmails}`;
