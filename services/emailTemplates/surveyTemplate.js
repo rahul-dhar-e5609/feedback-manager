f300ce72610382fa17202e6dc0971dd5a2607e65
IAmRDhar <rahul.dhar47@gmail.com>
2018-06-30 14:02:48 +0530

    modified template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -2,21 +2,33 @@ const keys = require('../../config/keys');
 module.exports = (survey) => {
   return `
     <html>
+      <head>
+        <style>
+        .container{
+          text-align:center;
+        }
+        .link{
+          padding:10% 2% 1% 2%;
+          text-decoration:none;
+          color:white;
+          background-color:#4CAF50!important;
+          border:1px solid #0f0
+        }
+        .link:hover{
+          color:#4CAF50;
+          background-color:white!important;
+        }
+        </style>
+      </head>
       <body>
         <div style="text-align:center">
-          <h3>I'd like your input!</h3>
-          <p>Please answer the following questions:</p>
+          <h3>${survey.subject}</h3>
           <p>${survey.body}</p>
-          // <div>
-          //   <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
-          // </div>
-          // <div>
-          //   <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
-          // </div>
-          <a href=""${keys.redirectDomain}/app/survey/${survey.id}">
-            <button>Take Survey</button>
-          </a>
-        </div>
+          <div class="container">
+            <a class="link" href="${keys.redirectDomain}/app/survey/${survey.id}">
+              Take Survey
+            </a>
+          </div>
       </body>
     </html>
   `;
